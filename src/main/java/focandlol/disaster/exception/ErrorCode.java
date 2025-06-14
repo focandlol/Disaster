2e5fa0cee3ca7d69a4e87d4aaeb8241e0ae2515e
focandlol <focandlol@naver.com>
2025-06-14 21:21:37 +0900

    feat: 댓글 구현

--- src/main/java/focandlol/disaster/exception/ErrorCode.java
+++ src/main/java/focandlol/disaster/exception/ErrorCode.java
@@ -17,7 +17,12 @@ public enum ErrorCode {
   EXPIRED_TOKEN("만료된 토큰입니다.", UNAUTHORIZED),
   INVALID_SIGNATURE("잘못된 시그니처입니다.", UNAUTHORIZED),
   UNSUPPORTED_TOKEN("지원하지 않는 토큰 형식입니다.", BAD_REQUEST),
+
   NO_TOKEN("토큰이 없습니다",BAD_REQUEST),
+  REPORT_NOT_FOUND("존재하지 않는 제보입니다", BAD_REQUEST),
+  MEMBER_NOT_FOUND("존재하지 않는 멤버입니다", BAD_REQUEST),
+  COMMENT_NOT_FOUND("존재하지 않는 댓글입니다",BAD_REQUEST),
+  ANOTHER_MEMBER("다른 멤버입니다", BAD_REQUEST),
   RECENT_DATE_NOT_FOUND("최근 날짜가 존재하지 않습니다",HttpStatus.INTERNAL_SERVER_ERROR);
 
 
